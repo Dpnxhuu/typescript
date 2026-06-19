9095ae4f1cbedbc0177cbb4870b432a5dac36db3
dpnxhuu <dpnxhuu@email.com>
2026-06-19 13:37:57 +0530

    Day-1 done learning ts

---
+++ Day-1/index.js
@@ -0,0 +1,7 @@
+"use strict";
+// let name: string = "Deepanshu";
+// console.log(name);
+// let age: number = 21;
+// console.log(age);
+Object.defineProperty(exports, "__esModule", { value: true });
+//# sourceMappingURL=index.js.map
